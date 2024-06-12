1fcb6a3bbf1877a294bda456606fbfaef007241d
Lakshan Roshana <131844311+lakshan-roshana@users.noreply.github.com>
2024-06-13 01:47:27 +0530

    Add files via upload

--- bubblesort.c
+++ bubblesort.c
@@ -1,4 +1,25 @@
 #include<stdio.h>
+void bubblesort(int arr[], int n){
+
+    //sorting process using bubble sort
+    int i,j,tempvalue;
+    for(i=0;i<n-1;i++){
+
+        for(j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                tempvalue = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tempvalue;
+            }
+        }
+    }
+
+    //print sorted array
+    printf("\nSorted Array => \n");
+    for(i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+}
 
 int main(){
     int n;
@@ -12,6 +33,29 @@ int main(){
         scanf("%d",&arr[i]);
     }
 
+    //call function
+    bubblesort(arr,n);
+}
+
+//---------------------------------------------------------------------------------------------------------------------------------
+//without function 
+/*
+
+#include<stdio.h>
+
+int main(){
+    int n;
+    printf("how many elements are entered: ");
+    scanf("%d",&n);
+    int arr[n],i;
+
+    //ask from user for array.
+    for(i=0;i<n;i++){
+        printf("element %d -> ",i+1);
+        scanf("%d",&arr[i]);
+    }
+
+    //sorting process using bubble sort
     int j,tempvalue;
     for(i=0;i<n-1;i++){
         for(j=0;j<n-i-1;j++){
@@ -31,3 +75,5 @@ int main(){
 
     return 0;
 }
+
+*/
